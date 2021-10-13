daf6854b1919b0f78ea3a4ff75d62591e431fbea
Iksang_J <89429506+iksang0110@users.noreply.github.com>
2021-10-13 15:39:43 +0900

    Initial Commit

---
+++ Clang/main.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  Clang
+//
+//  Created by 정익상 on 2021/10/13.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
