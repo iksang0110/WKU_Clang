3e96785ca432f064abc18b4731aceda0516c581e
Iksang_J <89429506+iksang0110@users.noreply.github.com>
2021-10-20 15:39:26 +0900

    ㅇ

---
+++ 고급프로그래밍언어/고급프로그래밍언어/main.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  고급프로그래밍언어
+//
+//  Created by 정익상 on 2021/10/20.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
